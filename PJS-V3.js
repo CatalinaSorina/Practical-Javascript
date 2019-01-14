372ffe6e253e5fc1387949a96619a60f9098f1e8
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-01-14 14:48:10 +0200

    displayTodoText()

--- PJS-V3.js
+++ PJS-V3.js
@@ -1,9 +1,18 @@
 
 let todoList = {
-    todos: ['item 1','item 2','item 3'],
+    todos: ['item 1',{todoText: "object 1",completed: true},'item 2','item 3', {todoText: "object 2",completed: false}],
     
     displayTodos: function() {
         console.log('My todos:',this.todos);
+    },
+
+    displayTodosText: function() {
+        console.log('My todo text:');
+        for (let i=0; i<this.todos.length; i++){
+            if (typeof this.todos[i] === "object"){
+                console.log(this.todos[i].todoText);
+            }
+        }
     },
 
     addTodo: function(todo) {
