2247ec44b98fbf0cb4a98a97078c4a17e88ef36e
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-01-14 14:58:23 +0200

    empty todo list

--- PJS-V3.js
+++ PJS-V3.js
@@ -3,15 +3,25 @@ let todoList = {
     todos: ['item 1',{todoText: "object 1",completed: true},'item 2','item 3', {todoText: "object 2",completed: false}],
     
     displayTodos: function() {
-        console.log('My todos:',this.todos);
+        if (this.todos.length === 0){
+            console.log ('Your todo list is empty!');
+        }else{
+            console.log('My todos:',this.todos);
+        }
     },
 
     displayTodosText: function() {
-        console.log('My todo text:');
+        let todoObj = [];
         for (let i=0; i<this.todos.length; i++){
             if (typeof this.todos[i] === "object"){
-                console.log(this.todos[i].todoText);
+                todoObj.push(this.todos[i].todoText);
             }
+        }
+
+        if (todoObj.length === 0){
+            console.log('The todo list have no object.');
+        }else{
+            console.log('My todos:',todoObj);
         }
     },
 
